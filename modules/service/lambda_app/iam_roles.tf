768ec1948ff48344a4dcbf54858b3a5038db9585
Richard Rhodes <rgrhodesdev@gmail.com>
2021-10-23 20:32:14 +0100

    Updated IAM role

--- modules/service/lambda_app/iam_roles.tf
+++ modules/service/lambda_app/iam_roles.tf
@@ -16,42 +16,4 @@ resource "aws_iam_role" "lambda_service_role" {
           ]
       })
 
-      inline_policy {
-
-          name = "S3_Policy"
-
-          policy = jsonencode({
-          Version = "2012-10-17"
-          Statement = [
-              {
-              Action   = [
-                  
-                  "logs:CreateLogGroup",
-                  "logs:CreateLogStream",
-                  "logs:PutLogEvents"
-                  
-                  ]
-              Effect   = "Allow"
-              Resource = "*"
-              },
-                              {
-              Action   = [
-
-                  "s3:Get*",
-                  "s3:List*"
-                  
-                  ]
-              Effect   = "Allow"
-              Resource = [
-
-                  "arn:aws:s3:::lambda-deployment-bucket-20211020/",
-                  "arn:aws:s3:::lambda-deployment-bucket-20211020/*"
-
-
-                  ]
-              },
-          ]
-          })
-      }
-
 }
